1ff109813294e438f9041d20f0ba17010030c30c
Al-Amin <alamin50cse.miu@gmail.com>
2026-07-02 11:08:53 +0600

    banner image add

--- src/main/Home/Carousel.tsx
+++ src/main/Home/Carousel.tsx
@@ -7,6 +7,12 @@ import "swiper/css/pagination"
 import { Pagination, Autoplay } from "swiper/modules"
 import Image from "next/image"
 
+const slides = [
+    { src: "/carousel/pizza.png", alt: "Castello Pizza" },
+    { src: "/carousel/caroselImgage2.png", alt: "Castello Special" },
+    { src: "/carousel/caroselimage3.png", alt: "Castello Offer" },
+]
+
 export default function Carousel() {
     return (
         <Swiper
@@ -19,11 +25,11 @@ export default function Carousel() {
             loop={true}
             className="w-full aspect-1920/631 md:aspect-auto md:h-full custom-swiper"
         >
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
+            {slides.map((slide, i) => (
                 <SwiperSlide key={i} className="relative w-full h-full overflow-hidden">
                     <Image
-                        src="/carousel/pizza.png"
-                        alt={`slide-${i}`}
+                        src={slide.src}
+                        alt={slide.alt}
                         fill
                         priority={i === 0}
                         sizes="(max-width: 640px) 640px, (max-width: 1024px) 1024px, 1440px"
