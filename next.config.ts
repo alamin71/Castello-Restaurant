88d2be22a53399bfea35a52361a72aa1dcf5fed3
Al-Amin <alamin50cse.miu@gmail.com>
2026-07-18 15:46:31 +0600

    menu product intriagation

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "castello-restaurants.s3.eu-north-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
